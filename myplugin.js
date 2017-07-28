b603783f70c60afc160b85cbcb9d99693cf59b0b
Brody Liao <brodyliao@shixianmengxiangderen.local>
2017-07-28 13:25:59 +0800

    make plugin

--- myplugin.js
+++ myplugin.js
@@ -1,5 +1,6 @@
 // MyPlugin.js
 let htmlToText = require('html-to-text');
+let fs = require('fs');
 function MyPlugin(options) {
   // Configure your plugin with options...
 }
@@ -9,10 +10,15 @@ MyPlugin.prototype.apply = function(compiler) {
   compiler.plugin('compilation', function(compilation) {
     console.log('The compiler is starting a new compilation...');
     compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
-      console.log(htmlPluginData);
-      console.log(require('./view/common.ejs'));
-      htmlPluginData.html = '<html>\n<head>\n' +  htmlPluginData.html + '\n</head>\n<body>\n12323\n</body>\n</html>'
-      callback(null, htmlPluginData);
+      let options = htmlPluginData.plugin.options;
+      fs.readFile(options.filename, 'utf8', function(err, html){
+          let titleReg = /<title>.*?<\/title>/ig;
+          let bodyReg = /<body>.*?<\/body>/ig;
+          let htmlArr = html.match(titleReg);
+          htmlPluginData.html = htmlPluginData.html.replace(titleReg, htmlArr[0]);
+          htmlPluginData.html = htmlPluginData.html.replace(bodyReg, '<body>' + html.replace(titleReg, '') + '</body>');
+          callback(null, htmlPluginData);
+      });
     });
   });
 
--- webpack.config.js
+++ webpack.config.js
@@ -69,7 +69,6 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'My App',
       filename: './view/index.html',
       template : './view/common.ejs'
     }),
