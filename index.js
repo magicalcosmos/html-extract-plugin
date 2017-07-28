60c0a368e90a715fda127354cb9fe0dd2df37384
Brody Liao <brodyliao@shixianmengxiangderen.local>
2017-07-28 14:07:44 +0800

    change name

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-// MyPlugin.js
-let htmlToText = require('html-to-text');
-let fs = require('fs');
-function MyPlugin(options) {
-  // Configure your plugin with options...
-}
+var htmlToText = require('html-to-text');
+var fs = require('fs');
 
-MyPlugin.prototype.apply = function(compiler) {
-  // ...
+function HtmlExtract(options) {}
+
+HtmlExtract.prototype.apply = function(compiler) {
   compiler.plugin('compilation', function(compilation) {
     console.log('The compiler is starting a new compilation...');
     compilation.plugin('html-webpack-plugin-before-html-processing', function(htmlPluginData, callback) {
-      let options = htmlPluginData.plugin.options;
+      var options = htmlPluginData.plugin.options;
       fs.readFile(options.filename, 'utf8', function(err, html){
-          let titleReg = /<title>.*?<\/title>/ig;
-          let bodyReg = /<body>.*?<\/body>/ig;
-          let htmlArr = html.match(titleReg);
+          var titleReg = /<title>.*?<\/title>/ig;
+          var bodyReg = /<body>.*?<\/body>/ig;
+          var htmlArr = html.match(titleReg);
           htmlPluginData.html = htmlPluginData.html.replace(titleReg, htmlArr[0]);
           htmlPluginData.html = htmlPluginData.html.replace(bodyReg, '<body>' + html.replace(titleReg, '') + '</body>');
           callback(null, htmlPluginData);
@@ -24,4 +21,4 @@ MyPlugin.prototype.apply = function(compiler) {
 
 };
 
-module.exports = MyPlugin;
+module.exports = HtmlExtract;
